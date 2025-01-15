7dda44eeeca38b1cb0128ccae30ff72f213bdf62
Taziiev Andrii <andrewtaziiev@gmail.com>
2025-01-15 11:48:24 +0200

    finished setting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Layout from './Layout';
+import ActorDetail from './pages/ActorDetail';
+import MovieDetail from './pages/MovieDetail';
+import Movies from './pages/Movies';
 
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Movies />,
+        },
+        {
+          path: '/movie/:id',
+          element: <MovieDetail />,
+        },
+
+        {
+          path: '/actor/:id',
+          element: <ActorDetail />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
