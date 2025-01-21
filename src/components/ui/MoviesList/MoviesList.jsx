5be5fddb27614f0f92d3d5474befeb3a655f80af
Taziiev Andrii <andrewtaziiev@gmail.com>
2025-01-21 17:07:05 +0200

    finished movie list

--- src/components/ui/MoviesList/MoviesList.jsx
+++ src/components/ui/MoviesList/MoviesList.jsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material';
+import { Pagination, Stack } from '@mui/material';
 import React from 'react';
 
 import MovieCard from '../MovieCard';
@@ -6,10 +6,20 @@ import MovieCard from '../MovieCard';
 export default function MoviesList({ movies, totalPages, page, setPage }) {
   return (
     <>
-      <Stack>
+      <Stack direction="row" justifyContent="center" flexWrap="wrap">
         {movies.map(movie => (
           <MovieCard key={movie.kinopoiskId} movie={movie} />
         ))}
+      </Stack>
+      <Stack alignItems="center">
+        <Pagination
+          count={totalPages}
+          variant="outlined"
+          color="primary"
+          size="large"
+          page={page}
+          onChange={(_, value) => setPage(value)}
+        />
       </Stack>
     </>
   );
