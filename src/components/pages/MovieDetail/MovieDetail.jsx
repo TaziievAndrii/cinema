e8cca07366648e4eedfc16a2c251b9595fc2bb8e
Taziiev Andrii <andrewtaziiev@gmail.com>
2025-02-02 16:29:27 +0200

    finished main section into  detail page

--- src/components/pages/MovieDetail/MovieDetail.jsx
+++ src/components/pages/MovieDetail/MovieDetail.jsx
@@ -1,5 +1,12 @@
-import { ArrowBack } from '@mui/icons-material';
-import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material';
+import { ArrowBack, Language, Movie } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  CircularProgress,
+  Grid,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -39,113 +46,142 @@ export default function MovieDetail() {
   }
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={3}>
-        <img
-          width="100%"
-          src={responseFilm.data.posterUrl}
-          alt={
-            responseFilm.data.nameOriginal
-              ? responseFilm.data.nameOriginal
-              : responseFilm.data.nameRu
-          }
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <Grid container>
-          <Grid xs={2}>
-            <Button
-              startIcon={<ArrowBack />}
-              size="large"
-              onClick={() => navigate(-1)}
-            ></Button>
-          </Grid>
-          <Grid xs={10} alignContent="center">
-            <Typography variant="h5">
-              {responseFilm.data.nameOriginal
+    <>
+      <Grid container spacing={2} mt={2}>
+        <Grid item xs={3}>
+          <img
+            width="100%"
+            src={responseFilm.data.posterUrl}
+            alt={
+              responseFilm.data.nameOriginal
                 ? responseFilm.data.nameOriginal
-                : responseFilm.data.nameRu}
-            </Typography>
-          </Grid>
+                : responseFilm.data.nameRu
+            }
+          />
         </Grid>
-        <Grid container>
-          <Grid xs={6}>
-            <Typography>Year</Typography>
-          </Grid>
-          <Grid xs={6}>
-            <Typography gutterBottom>{responseFilm.data.year}</Typography>
-          </Grid>
-
-          <Grid xs={6}>
-            <Typography>Country</Typography>
-          </Grid>
-          <Grid xs={6}>
-            {responseFilm.data.countries.map(({ country }) => (
-              <Typography key={country} gutterBottom>
-                {country}
+        <Grid item xs={6}>
+          <Grid container>
+            <Grid xs={2}>
+              <Button
+                startIcon={<ArrowBack />}
+                size="large"
+                onClick={() => navigate(-1)}
+              ></Button>
+            </Grid>
+            <Grid xs={10} alignContent="center">
+              <Typography variant="h5">
+                {responseFilm.data.nameOriginal
+                  ? responseFilm.data.nameOriginal
+                  : responseFilm.data.nameRu}
               </Typography>
-            ))}
+            </Grid>
           </Grid>
+          <Grid container>
+            <Grid xs={6}>
+              <Typography>Year</Typography>
+            </Grid>
+            <Grid xs={6}>
+              <Typography gutterBottom>{responseFilm.data.year}</Typography>
+            </Grid>
 
-          <Grid xs={6}>
-            <Typography>Genre</Typography>
-          </Grid>
-          <Grid xs={6}>
-            {responseFilm.data.genres.map(({ genre }) => (
-              <Typography key={genre} gutterBottom>
-                {genre}
-              </Typography>
-            ))}
-          </Grid>
-
-          <Grid xs={6}>
-            <Typography>Directors</Typography>
-          </Grid>
-          <Grid xs={6}>
-            {responseStaff.data
-              .filter(el => el.professionKey === 'DIRECTOR')
-              .map(staff => (
-                <Typography key={staff.staffId} gutterBottom>
-                  {staff.nameEn}
+            <Grid xs={6}>
+              <Typography>Country</Typography>
+            </Grid>
+            <Grid xs={6}>
+              {responseFilm.data.countries.map(({ country }) => (
+                <Typography key={country} gutterBottom>
+                  {country}
                 </Typography>
               ))}
-          </Grid>
+            </Grid>
 
-          <Grid xs={6}>
-            <Typography>Time</Typography>
-          </Grid>
-          <Grid xs={6}>
-            <Typography gutterBottom>
-              {responseFilm.data.filmLength} min
-            </Typography>
-          </Grid>
+            <Grid xs={6}>
+              <Typography>Genre</Typography>
+            </Grid>
+            <Grid xs={6}>
+              {responseFilm.data.genres.map(({ genre }) => (
+                <Typography key={genre} gutterBottom>
+                  {genre}
+                </Typography>
+              ))}
+            </Grid>
 
-          <Grid xs={6}>
-            <Typography>Description</Typography>
-          </Grid>
-          <Grid xs={6}>
-            <Typography gutterBottom>
-              {responseFilm.data.description
-                ? responseFilm.data.description
-                : '🚀 No description, just vibes'}
-            </Typography>
+            <Grid xs={6}>
+              <Typography>Directors</Typography>
+            </Grid>
+            <Grid xs={6}>
+              {responseStaff.data
+                .filter(el => el.professionKey === 'DIRECTOR')
+                .map(staff => (
+                  <Typography key={staff.staffId} gutterBottom>
+                    {staff.nameEn}
+                  </Typography>
+                ))}
+            </Grid>
+
+            <Grid xs={6}>
+              <Typography>Time</Typography>
+            </Grid>
+            <Grid xs={6}>
+              <Typography gutterBottom>
+                {responseFilm.data.filmLength} min
+              </Typography>
+            </Grid>
+
+            <Grid xs={6}>
+              <Typography>Description</Typography>
+            </Grid>
+            <Grid xs={6}>
+              <Typography gutterBottom>
+                {responseFilm.data.description
+                  ? responseFilm.data.description
+                  : '🚀 No description, just vibes'}
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={3}>
-        <Typography gutterBottom variant="h6">
-          🎭 Starring:
-        </Typography>
+        <Grid item xs={3}>
+          <Typography gutterBottom variant="h6">
+            🎭 Starring:
+          </Typography>
 
-        {responseStaff.data
-          .filter(el => el.professionKey === 'ACTOR')
-          .slice(0, 10)
-          .map(staff => (
-            <Typography key={staff.staffId} gutterBottom>
-              {staff.nameEn}
-            </Typography>
-          ))}
+          {responseStaff.data
+            .filter(el => el.professionKey === 'ACTOR')
+            .slice(0, 10)
+            .map(staff => (
+              <Typography key={staff.staffId} gutterBottom>
+                {staff.nameEn}
+              </Typography>
+            ))}
+        </Grid>
       </Grid>
-    </Grid>
+      <Grid
+        container
+        spacing={2}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Grid item xs={12}>
+          <ButtonGroup variant="outlined" size="small">
+            <Button
+              target="_blank"
+              href={responseFilm.data.webUrl}
+              endIcon={<Language />}
+            >
+              Kinopoisk
+            </Button>
+            <Button
+              target="_blank"
+              href={`https://www.imdb.com/title/${responseFilm.data.imdbId}`}
+              endIcon={<Movie />}
+            >
+              IMDB
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    </>
   );
 }
